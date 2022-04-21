1493add0ae5fb3c606269bfe9f32f813946e25e3
Beqa <beqa.tabunidze@icloud.com>
2022-04-21 13:17:50 +0400

    Initial Commit

---
+++ Stanford/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stanford
+//
+//  Created by Beqa Tabunidze on 21.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
